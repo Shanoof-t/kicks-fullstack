0cb21ca90f629d18ac5787300049ed3a759abd4d
Shanoof <shanoof.t@deepnetsoft.com>
2026-07-19 15:45:40 +0530

    fix cors

--- server/src/loaders/express.js
+++ server/src/loaders/express.js
@@ -8,7 +8,7 @@ import morgan from "../config/morgan.js";
 export default ({ app, express }) => {
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: true,
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
     })
